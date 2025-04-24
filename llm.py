803bf0ab9c9f88a7415c7a19d5274cc145f8a169
Adrianna Koppes <ask033240@gmail.com>
2025-04-24 10:34:20 -0700

    Some prompt modifications and now only the first answer in the LLM generated list is checked

--- llm.py
+++ llm.py
@@ -31,14 +31,15 @@ for i in range(questions_to_skip*4, len(questions), 4):
     cur = {"category":questions[i].strip(), "prompt":questions[i+1].strip(), "answer":[x.lower() for x in questions[i+2].strip().split("|")]}
     queries.append(cur)
 
-system_msg = "You are a Jeopardy expert and you are tasked with answering the question by providing only the wikipedia page title.\n" \
-"Do not answer in a question format. You are to not speculate and only refer to wikipedia pages for your answers. Your answers must be relevant to the given category"
+system_msg = "You are a Jeopardy expert and you are tasked with answering the question by providing only the Wikipedia page title.\n" \
+"Do not answer in a question format. You are to not speculate and only refer to Wikipedia pages for your answers. Your answers must be relevant to the given category"
 
 
-msg = "Give me a list of the 10 best possible answers in a CSV format. (EG. The Washington Post,The New York Times,...).\n" \
+msg = "Provide a comma-separated list of the 10 best possible answers to the given question. (EG. The Washington Post,The New York Times,...). Your list must contain exactly 10 answers.\n" \
 "Only output the answers, do not add any other filler text such as 'Here are the possible answers:'.\n" \
-"Ensure that you do not include duplicate answers.\n" \
-"If the category has an instruction in parentheses, ensure that you follow it's instructions as it specifies what kind of answer it should be. (Ex. We'll give you the museum, you give us the state. ONLY GIVE THE STATE)\n" \
+"When creating your list of answers, ensure that you follow these guidelines:\n" \
+"Ensure that you do not include more than one of the same answer at any point in your list.\n" \
+"If the category includes an instruction in parentheses, starting with 'Alex: ', ensure that you follow its instructions as it specifies what kind of answer it should be. For example, if the instructions say 'Alex: We've given you the monument. You give us the city.', then you must give the city in which the monument in the question is located.\n" \
 "Make sure the answers are all on one line and does not include any quotes or delimiters besides the commas.\n" \
 "The question is delimited by ''' and the category is delimited for ---.\n" \
 "---{}--- '''{}'''"
@@ -62,12 +63,15 @@ for query in tqdm(queries):
         max_new_tokens=256,
         pad_token_id=128001  
     )
+    print(outputs[0]["generated_text"][-1]["content"])
     answers = outputs[0]["generated_text"][-1]["content"].split(",")
     answers = [x.strip().strip("\"").lower() for x in answers]
 
     
     
-    if len(set(query["answer"]) & set(answers)) != 0:
+    #if len(set(query["answer"]) & set(answers)) != 0:
+    if answers[0] in query["answer"]:
+        # first answer given by the LLM is correct
         print('Correct')
         correct += 1
     # file.write(f"{query[0]}\n{query[1]}\n{result}\n\n")
@@ -75,7 +79,7 @@ for query in tqdm(queries):
     with open("cur_question.txt", "w") as f:
         f.write(str(questions_to_skip+i) + "\n" + str(correct))
     
-print(f"Accuracy: {(correct/100)*100}")
+print(f"Precision: {correct/100}")      # Precision = (# of correct answers retrieved) / (# total answers retrieved)
 
 
 
