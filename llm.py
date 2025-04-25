308304e07a90fbb0d5d1084d29783410dbe74bad
Adrianna Koppes <ask033240@gmail.com>
2025-04-24 22:12:21 -0700

    Ranking results and discarding results that are already in the question not working yet

--- llm.py
+++ llm.py
@@ -22,8 +22,8 @@ else:
     questions_to_skip = 0
     correct = 0
 
-questions_to_skip = 0 if questions_to_skip == 100 else questions_to_skip
 correct = 0 if questions_to_skip == 100 else correct
+questions_to_skip = 0 if questions_to_skip == 100 else questions_to_skip
 
 questions = open("questions.txt").readlines()
 queries = []
@@ -32,16 +32,17 @@ for i in range(questions_to_skip*4, len(questions), 4):
     queries.append(cur)
 
 system_msg = "You are a Jeopardy expert and you are tasked with answering the question by providing only the Wikipedia page title.\n" \
-"Do not answer in a question format. You are to not speculate and only refer to Wikipedia pages for your answers. Your answers must be relevant to the given category"
+"Do not answer in a question format. You are to not speculate and you must refer only to Wikipedia pages for your answers. Your answers must be relevant to the given category."
 
 
-msg = "Provide a comma-separated list of the 10 best possible answers to the given question. (EG. The Washington Post,The New York Times,...). Your list must contain exactly 10 answers.\n" \
+msg = "Provide a comma-separated list of the 10 best possible answers to the given question. (EG. The Washington Post,The New York Times,...). The list must be ordered such that the answer that best answers the question is the first item in the list, the second best answer is the second item in the list, and so on. Your list must contain exactly 10 answers.\n" \
 "Only output the answers, do not add any other filler text such as 'Here are the possible answers:'.\n" \
 "When creating your list of answers, ensure that you follow these guidelines:\n" \
-"Ensure that you do not include more than one of the same answer at any point in your list.\n" \
+"Ensure that all answers in your list are unique, that is, each answer in your list should appear exactly once.\n" \
+"The questions you are answering provide clues to the answers which you are seeking, and thus use words different from those of an appropriate answer. Ensure that all of the answers you give answer the question rather than repeating a part of it. For example, if the question is 'George Washington was born in what is now this state.', 'George Washington' would be a bad answer. Additionally, as another example, if the question is 'This person is the editor-in-chief of The New York Times.', then 'The New York Times', 'New York Times', 'N.Y. Times', and 'NY Times' would all be bad answers.\n" \
 "If the category includes an instruction in parentheses, starting with 'Alex: ', ensure that you follow its instructions as it specifies what kind of answer it should be. For example, if the instructions say 'Alex: We've given you the monument. You give us the city.', then you must give the city in which the monument in the question is located.\n" \
 "Make sure the answers are all on one line and does not include any quotes or delimiters besides the commas.\n" \
-"The question is delimited by ''' and the category is delimited for ---.\n" \
+"The question is delimited by ''' and the category is delimited by ---.\n" \
 "---{}--- '''{}'''"
 
 # file = open("answers.txt", "w")
